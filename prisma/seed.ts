90a23d93ddea7bc2eb3beb1f28f7ff760d007b12
mohyaldeentellawi1 <mohyaldeentellawi1@gmail.com>
2026-05-21 00:43:30 +0300

    feat: integrate Prisma with PostgreSQL and set up initial database schema
    - Added Prisma dependencies to package.json.
    - Created prisma.ts for Prisma client initialization with PostgreSQL adapter.
    - Added prisma.config.ts for Prisma configuration.
    - Created initial migration files for the database schema including Project, ProjectTag, and ProjectMedia tables.
    - Defined enums for ProjectType and ProjectMediaType in the schema.
    - Created seed.ts for future data seeding.

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+// import { PrismaClient } from "@/app/generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import "dotenv/config";
+
+// // const adapter = new PrismaPg({
+// //   connectionString: process.env.DATABASE_URL,
+// // });
+// // const prisma = new PrismaClient({
+// //   adapter,
+// // });
+
+// export async function main() {
+//   //   for (const u of userData) {
+//   //     await prisma.user.create({ data: u });
+//   //   }
+// }
+
+// main()
+//   .then(() => {
+//     console.log("Seeding complete.");
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   });
+
+// THIS Script
+//npx prisma db seed
