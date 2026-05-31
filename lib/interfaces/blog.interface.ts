c887a43d21e35ea4287b72c5fe0495d5c4d2ee4a
mohyaldeentellawi1 <mohyaldeentellawi1@gmail.com>
2026-05-31 12:41:47 +0300

    feat: add Post interface for blog data structure

---
+++ lib/interfaces/blog.interface.ts
@@ -0,0 +1,13 @@
+import { BlogType } from "@/app/generated/prisma/enums";
+
+export interface Post {
+  id: number;
+  type: BlogType;
+  title: string;
+  titleEn?: string;
+  content: string;
+  contentEn?: string;
+  readerCount: number;
+  createdAt: Date; // ISO date string
+  updatedAt: Date; // ISO date string
+}
