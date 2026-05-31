2c7937d2d841f7fecb0a15ce0d2daddc4bc9df7d
mohyaldeentellawi1 <mohyaldeentellawi1@gmail.com>
2026-05-31 13:22:08 +0300

    feat: refactor GET function parameter to improve readability

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ request: req }: { request: NextRequest }) {
+export async function GET(req: NextRequest) {
   const authHeader = req.headers.get("Authorization");
 
   const cronSecret = process.env.CRON_SECRET;
